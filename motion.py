539d3adde4660967c7d24986209d57bc662b344d
013 <tswizzle@live.co.uk>
2014-03-27 08:41:20 +0000

    Much better

--- motion.py
+++ motion.py
@@ -11,30 +11,18 @@ class detect:
 	def __init__(self):
 		self.ips = ['192.168.0.61']
 		self.commands = []
-		self.commands_1 = []
-		self.commands_2 = []
 		self.FNULL = open(os.devnull, 'w')
+		self.kill = False # If true exit processes
 
 	def takeImages(self, ip):
 		cid = ip.split('.')[-1] # Get last octet and use as an id
-		#self.commands_1.append('avconv -i rtsp://admin:linnit@{0}/Streaming/Channels/2 -f image2 -vframes 1 1_{1}.jpg'.format(ip, cid))
-		self.commands.append('ffmpeg -i rtsp://admin:linnit@{0}/Streaming/Channels/2 -ss 00:00:01.500 -t 10 -r 1 -f image2 image_{1}-%2d.jpg'.format(ip, cid))
-		#self.commands_2.append('avconv -i rtsp://admin:linnit@{0}/Streaming/Channels/2 -f image2 -vframes 1 2_{1}.jpg'.format(ip, cid))
+		self.commands.append('ffmpeg -i rtsp://admin:linnit@{0}/Streaming/Channels/2 -t 120 -r 1 -f image2 image_{1}-%3d.jpg'.format(ip, cid))
 		
-		# Take 10 images
-		#processes = [subprocess.Popen(cmd, shell=True, stdout=self.FNULL, stderr=subprocess.STDOUT) for cmd in self.commands]
-		processes = [subprocess.Popen(cmd, shell=True) for cmd in self.commands]
-		# Take first image
-		#processes_1 = [subprocess.Popen(cmd, shell=True, stdout=self.FNULL, stderr=subprocess.STDOUT) for cmd in self.commands_1]
-		# Sleep 1 seconds
-		#time.sleep(1)
-		# Take the second image
-		#processes_2 = [subprocess.Popen(cmd, shell=True, stdout=self.FNULL, stderr=subprocess.STDOUT) for cmd in self.commands_2]
-		
+		# Take stream of images
+		processes = [subprocess.Popen(cmd, shell=True, stdout=self.FNULL, stderr=subprocess.STDOUT) for cmd in self.commands]
+		#processes = [subprocess.Popen(cmd, shell=True) for cmd in self.commands]
 		# Wait for the process to finish
 		for p in processes: p.wait()
-		#for p in processes_1: p.wait()
-		#for p in processes_2: p.wait()
 
 	def image_entropy(self, img):
 		w,h = img.size
@@ -48,16 +36,13 @@ class detect:
 		red = '\033[91m'
 		green = '\033[92m'
 		reset = '\033[0m'
-		self.takeImages(cam_ip)
 		
-		for i in range(1,10,2):
-			if i < 9:
-				imgno1 = '0'+str(i)
-				imgno2 = '0'+str(i+1)
-			elif i == 9:
-				imgno1 = '0'+str(i)
-				imgno2 = '10'
-			
+		for i in range(1,120,2):
+			if self.kill == True: return 0
+			time.sleep(2)
+			imgno1, imgno2 = "%03d" % (i,), "%03d" % (i+1,)
+			print imgno1, imgno2			
+
 			img1 = Image.open('image_{0}-{1}.jpg'.format(cam_ip.split('.')[-1], imgno1) )
 			img2 = Image.open('image_{0}-{1}.jpg'.format(cam_ip.split('.')[-1], imgno2) )
 	
@@ -72,8 +57,8 @@ class detect:
 				print 'Motion detected - saving image to {0}_{1}.png'.format(cam_ip.split('.')[-1], int(time.time()) )
 			print "IP: {0}\t Entropy: {1}{2}{3}".format(cam_ip, colour, image_ent, reset)
 			# Remove images
-			#os.unlink('1_{0}.jpg'.format(cam_ip.split('.')[-1]))
-			#os.unlink('2_{0}.jpg'.format(cam_ip.split('.')[-1]))
+			os.unlink('image_{0}-{1}.jpg'.format(cam_ip.split('.')[-1], imgno1))
+			os.unlink('image_{0}-{1}.jpg'.format(cam_ip.split('.')[-1], imgno2))
 		
 
 def main():
@@ -83,6 +68,8 @@ def main():
 		try:
 			threads = []
 			for ip in d.ips:
+				t = Thread(target=d.takeImages, args=(ip,))
+				threads.append(t)
 				t = Thread(target=d.compare, args=(ip,))
 				threads.append(t)
 			
@@ -92,6 +79,8 @@ def main():
 			#det = False
 		except KeyboardInterrupt:
 			print "Keyboard Interrupt received"
+			det = False
+			d.kill = True
 			for x in threads:
 				x.kill_received = True 
 			sys.exit()
